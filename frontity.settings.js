e9335de816f5c815e94e11fbcae9ed1cbc4a900e
smartDevel <57102149+smartDevel@users.noreply.github.com>
2020-06-04 20:27:34 +0200

    mars-theme-activated

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,92 +1,77 @@
 const settings = {
-  name: "ways4euwp-01",
-  state: {
-    frontity: {
-      url: "https://ways4eu.wordpress.com/",
-      title: "Ways4eu WordPress.com Blog",
-      description: "WordPress Site in Frontity development",
-    },
+  "name": "ways4euwp-01",
+  "state": {
+    "frontity": {
+      "url": "https://ways4eu.wordpress.com/",
+      "title": "Ways4eu WordPress.com Blog",
+      "description": "WordPress Site in Frontity development"
+    }
   },
-  packages: [
+  "packages": [
     {
-      name: "@frontity/twentytwenty-theme",
-      state: {
-          theme: {
-              menu: [
-                  [
-                    "Home",
-                    "/"
-                  ],
-                  [
-                    "Hot-React",
-                    "/category/hot-react"
-                  ],
-                  [
-                    "Tips&Tricks",
-                    "/category/tips_tricks"
-                  ],
-                  [
-                    "ReadingList",
-                    "/category/readinglist"
-                  ],
-                  [
-                    "Education",
-                    "/category/education"
-                  ], 
-                  [
-                    "Javascript",
-                    "/tag/javascript"
-                  ],                                    
-                  [
-                    "Wikipedia",
-                    "/category/wikipedia"
-                  ],
-                  [
-                    "NASA",
-                    "/category/nasa"
-                  ],            
-                  [
-                    "Art",
-                    "/tag/art"
-                  ],                                                            
-                  [
-                    "Music",
-                    "/tag/music"
-                  ]            
-                ],
-              colors: {
-                //primary: "#E6324B",
-                primary: "#46614f",
-                //headerBg: "#ffffff",
-                headerBg: "#f1f5eb",
-                //footerBg: "#ffffff",
-                footerBg: "#f1f5eb",
-                bodyBg: "#e8e6e1",
-                /* bodyBg: "#f5efe0" */
-              },
-              showSearchInHeader: true,
-              showAllContentOnArchive: false,
-              featuredMedia: {
-                showOnArchive: true,
-                showOnPost: true
-              },
-              autoPreFetch: "all",
-              fontSets: "us-ascii"
-            }
-      },
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Hot-React",
+              "/category/hot-react"
+            ],
+            [
+              "Tips&Tricks",
+              "/category/tips_tricks"
+            ],
+            [
+              "Reading List",
+              "/category/readinglist"
+            ],
+            [
+              "Education",
+              "/category/education"
+            ], 
+            [
+              "Javascript",
+              "/tag/javascript"
+            ],                                    
+            [
+              "Wikipedia",
+              "/category/wikipedia"
+            ],
+            [
+              "NASA",
+              "/category/nasa"
+            ],            
+            [
+              "Art",
+              "/tag/art"
+            ],                                                            
+            [
+              "Music",
+              "/tag/music"
+            ]            
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
     },
     {
-      name: "@frontity/wp-source",
-      state: {
-        source: {
-          api:
-            "https://public-api.wordpress.com/wp/v2/sites/ways4eu.wordpress.com",
-        },
-      },
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://public-api.wordpress.com/wp/v2/sites/ways4eu.wordpress.com"
+        }
+      }
     },
     "@frontity/tiny-router",
-    "@frontity/html2react",
-  ],
+    "@frontity/html2react"
+  ]
 };
 
 export default settings;
