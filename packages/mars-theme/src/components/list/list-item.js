d899f9fff77021c946389c4d8f980883055bea2d
smartDevel <57102149+smartDevel@users.noreply.github.com>
2020-06-08 11:47:58 +0200

    changed-renderfullview-content

--- packages/mars-theme/src/components/list/list-item.js
+++ packages/mars-theme/src/components/list/list-item.js
@@ -20,6 +20,7 @@ const Item = ({ state, item }) => {
       rndExcerpt = true;
     }
   }
+
   return (
     <article>
       <Link link={item.link}>
@@ -56,7 +57,7 @@ const Item = ({ state, item }) => {
 
       {/* If fullViewMode enabled we render the whole content /// Wenn fullView aktiv dann immer kompletten content anzeigen */}
       {state.theme.viewmode === "fullView" && (
-        <Excerpt dangerouslySetInnerHTML={{ __html: item.content.rendered }} />
+        <Content dangerouslySetInnerHTML={{ __html: item.content.rendered }} />
       )}
     </article>
   );
@@ -91,4 +92,125 @@ const PublishDate = styled.span`
 const Excerpt = styled.div`
   line-height: 1.6em;
   color: rgba(12, 17, 43, 0.8);
+`;
+
+const Content = styled.div`
+  color: rgba(12, 17, 43, 0.8);
+  word-break: break-word;
+
+  * {
+    max-width: 100%;
+  }
+
+  p {
+    line-height: 1.6em;
+  }
+
+  img {
+    width: 100%;
+    object-fit: cover;
+    object-position: center;
+  }
+
+  figure {
+    margin: 24px auto;
+    /* next line overrides an inline style of the figure element. */
+    width: 100% !important;
+
+    figcaption {
+      font-size: 0.7em;
+    }
+  }
+
+  iframe {
+    display: block;
+    margin: auto;
+  }
+
+  blockquote {
+    margin: 16px 0;
+    background-color: rgba(0, 0, 0, 0.1);
+    border-left: 4px solid rgba(12, 17, 43);
+    padding: 4px 16px;
+  }
+
+  a {
+    color: rgb(31, 56, 197);
+    text-decoration: underline;
+  }
+
+  /* Input fields styles */
+
+  input[type="text"],
+  input[type="email"],
+  input[type="url"],
+  input[type="tel"],
+  input[type="number"],
+  input[type="date"],
+  textarea,
+  select {
+    display: block;
+    padding: 6px 12px;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: 4px;
+    outline-color: transparent;
+    transition: outline-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    margin: 8px 0 4px 0;
+
+    &:focus {
+      outline-color: #1f38c5;
+    }
+  }
+
+  input[type="submit"] {
+    display: inline-block;
+    margin-bottom: 0;
+    font-weight: 400;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    -ms-touch-action: manipulation;
+    touch-action: manipulation;
+    cursor: pointer;
+    background-image: none;
+    border: 1px solid #1f38c5;
+    padding: 12px 36px;
+    font-size: 14px;
+    line-height: 1.42857143;
+    border-radius: 4px;
+    color: #fff;
+    background-color: #1f38c5;
+  }
+
+  /* WordPress Core Align Classes */
+
+  @media (min-width: 420px) {
+    img.aligncenter,
+    img.alignleft,
+    img.alignright {
+      width: auto;
+    }
+
+    .aligncenter {
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+    .alignright {
+      float: right;
+      margin-left: 24px;
+    }
+
+    .alignleft {
+      float: left;
+      margin-right: 24px;
+    }
+  }
 `;
