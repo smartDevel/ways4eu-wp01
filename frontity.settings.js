448d8255317a13ee0aaa8ee11799aff3241411de
smartDevel <57102149+smartDevel@users.noreply.github.com>
2022-12-29 00:07:00 +0100

    20221228_changedTheme

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,90 +1,92 @@
 const settings = {
-  "name": "ways4euwp-01",
-  "state": {
-    "frontity": {
-      "url": "https://ways4eu.wordpress.com/",
-      "title": "Ways4eu WordPress.com Blog",
-      "description": "SPA View of ways4eu.wordpress.com"
-    }
+  name: "ways4euwp-01",
+  state: {
+    frontity: {
+      url: "https://ways4eu.wordpress.com/",
+      title: "Ways4eu WordPress.com Blog",
+      description: "WordPress Site in Frontity development",
+    },
   },
-  "packages": [
+  packages: [
     {
-      "name": "@frontity/mars-theme",
-      "state": {
-        "theme": {
-          "menu": [
-            [
-              "Home",
-              "/"
-            ],
-            [
-              "Rhymes",
-              "/tag/rhymes"
-            ],
-            [
-              "Quotes",
-              "/tag/brainyquote"
-            ],
-            [
-              "Music",
-              "/tag/music"
-            ],
-            [
-              "Fun",
-              "/tag/fun"
-            ],
-            [
-              "Interesting",
-              "/tag/interesting"
-            ],
-            [
-              "NASA",
-              "/category/nasa"
-            ],                                           
-            [
-              "Hot-React",
-              "/category/hot-react"
-            ],
-            [
-              "Tips&Tricks",
-              "/category/tips_tricks"
-            ],
-            [
-              "Reading-List",
-              "/category/readinglist"
-            ],
-            [
-              "Education",
-              "/category/education"
-            ], 
-            [
-              "Javascript",
-              "/tag/javascript"
-            ],                                    
-            [
-              "Wikipedia",
-              "/category/wikipedia"
-            ]                                                                                         
-          ],
-          showSearchInHeader: true,
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
-          }
-        }
-      }
+      name: "@frontity/twentytwenty-theme",
+      state: {
+          theme: {
+              menu: [
+                  [
+                    "Home",
+                    "/"
+                  ],
+                  [
+                    "Hot-React",
+                    "/category/hot-react"
+                  ],
+                  [
+                    "Tips&Tricks",
+                    "/category/tips_tricks"
+                  ],
+                  [
+                    "ReadingList",
+                    "/category/readinglist"
+                  ],
+                  [
+                    "Education",
+                    "/category/education"
+                  ], 
+                  [
+                    "Javascript",
+                    "/tag/javascript"
+                  ],                                    
+                  [
+                    "Wikipedia",
+                    "/category/wikipedia"
+                  ],
+                  [
+                    "NASA",
+                    "/category/nasa"
+                  ],            
+                  [
+                    "Art",
+                    "/tag/art"
+                  ],                                                            
+                  [
+                    "Music",
+                    "/tag/music"
+                  ]            
+                ],
+              colors: {
+                //primary: "#E6324B",
+                primary: "#46614f",
+                //headerBg: "#ffffff",
+                headerBg: "#f1f5eb",
+                //footerBg: "#ffffff",
+                footerBg: "#f1f5eb",
+                bodyBg: "#e8e6e1",
+                /* bodyBg: "#f5efe0" */
+              },
+              showSearchInHeader: true,
+              showAllContentOnArchive: false,
+              featuredMedia: {
+                showOnArchive: true,
+                showOnPost: true
+              },
+              autoPreFetch: "all",
+              fontSets: "us-ascii"
+            }
+      },
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "api": "https://public-api.wordpress.com/wp/v2/sites/ways4eu.wordpress.com"
-        }
-      }
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          api:
+            "https://public-api.wordpress.com/wp/v2/sites/ways4eu.wordpress.com",
+        },
+      },
     },
     "@frontity/tiny-router",
-    "@frontity/html2react"
-  ]
+    "@frontity/html2react",
+  ],
 };
 
 export default settings;
---
+++ frontity.settings_20221228.js
@@ -0,0 +1,90 @@
+const settings = {
+  "name": "ways4euwp-01",
+  "state": {
+    "frontity": {
+      "url": "https://ways4eu.wordpress.com/",
+      "title": "Ways4eu WordPress.com Blog",
+      "description": "SPA View of ways4eu.wordpress.com"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Rhymes",
+              "/tag/rhymes"
+            ],
+            [
+              "Quotes",
+              "/tag/brainyquote"
+            ],
+            [
+              "Music",
+              "/tag/music"
+            ],
+            [
+              "Fun",
+              "/tag/fun"
+            ],
+            [
+              "Interesting",
+              "/tag/interesting"
+            ],
+            [
+              "NASA",
+              "/category/nasa"
+            ],                                           
+            [
+              "Hot-React",
+              "/category/hot-react"
+            ],
+            [
+              "Tips&Tricks",
+              "/category/tips_tricks"
+            ],
+            [
+              "Reading-List",
+              "/category/readinglist"
+            ],
+            [
+              "Education",
+              "/category/education"
+            ], 
+            [
+              "Javascript",
+              "/tag/javascript"
+            ],                                    
+            [
+              "Wikipedia",
+              "/category/wikipedia"
+            ]                                                                                         
+          ],
+          showSearchInHeader: true,
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://public-api.wordpress.com/wp/v2/sites/ways4eu.wordpress.com"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
